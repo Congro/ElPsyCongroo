d345bcd7d0cfb70e34ec09f7d5d1ad773a9450a3
Congro <127646130+Congro@users.noreply.github.com>
2023-03-13 22:46:29 -0600

    Update CongroHub.lua

--- CongroHub.lua
+++ CongroHub.lua
@@ -1,4 +1,4 @@
 if game.PlaceId == 8540346411 then
 --Rebrith Legends X
-loadstring("https://raw.githubusercontent.com/Congro/Rebirth-Champions-X/main/lua")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Congro/Rebirth-Champions-X/main/lua", true))()
 end
